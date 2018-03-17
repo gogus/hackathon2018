9470523274680310def61629d0749c112212ab1d
rowebsoft <rowebsoft@gmail.com>
2018-03-17 08:02:21 +0000

    modified to 1 result

--- src/Action/GetUserCar.php
+++ src/Action/GetUserCar.php
@@ -29,7 +29,7 @@ class GetUserCar
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args = [])
     {
         /** @var Response $response */
-        $user = $this->table->where('user_id', '=', $args['userId'])->get();
+        $user = $this->table->where('user_id', '=', $args['userId'])->get()->first();
 
         return $response->withJson($user);
     }
