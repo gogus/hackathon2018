16349bffea17f769d6d1272b27b099c4711033ce
Yuri Golikov <meetmatt@ya.ru>
2018-03-17 16:39:34 +0100

    Fixed car sharing bonus

--- src/RewardEngine/Rule/ParticipatingInCarShare.php
+++ src/RewardEngine/Rule/ParticipatingInCarShare.php
@@ -5,12 +5,13 @@ namespace Gtw\RewardEngine\Rule;
 use Gtw\RewardEngine\Bonus;
 use Gtw\RewardEngine\ChallengingConditions;
 use Gtw\RewardEngine\RideOption;
+use Gtw\RewardEngine\TransportType;
 
 class ParticipatingInCarShare extends AbstractBonusRewardRule
 {
     public function getReward(RideOption $rideOption, ChallengingConditions $conditions)
     {
-        if ($rideOption->getTransportType()) {
+        if ($rideOption->getTransportType() === TransportType::SHARED_CAR) {
             return new Bonus('Participating in car sharing', 30);
         }
 
