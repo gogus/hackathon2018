7ca19d7ea1f2c4fc193179987a930554d7860886
rowebsoft <rowebsoft@gmail.com>
2018-03-17 00:17:47 +0000

    added address endpoint

--- app/routes.php
+++ app/routes.php
@@ -10,5 +10,12 @@ return [
     'get-user-by-username' => [
         'pattern' => '/user/{username}',
         'action' => \Gtw\Action\GetUserByUsername::class
+    ],
+
+    'get-user-address' => [
+        'pattern' => '/user/address/{userId}',
+        'action' => \Gtw\Action\GetUserAddress::class
     ]
+
+
 ];
