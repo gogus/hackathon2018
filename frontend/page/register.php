036b29358fa17247df500718a4b207ec775a0523
Mikolaj Gogula <mikolaj.gogula@gmail.com>
2018-03-17 13:22:09 +0100

    Added registration.

--- frontend/page/register.php
+++ frontend/page/register.php
@@ -13,18 +13,18 @@ require_once __DIR__ . '/../header.php';
         </div>
 
         <h4 class="mb-3">Details</h4>
-        <form class="needs-validation" method="post" action="/api/user/register" novalidate>
+        <form autocomplete="off" role="presentation" class="needs-validation" id="form-register" method="post" action="/api/user/register" novalidate>
             <div class="row">
                 <div class="col-md-6 mb-3">
                     <label for="firstName">First name</label>
-                    <input name="first_name" type="text" class="form-control" id="firstName" placeholder="" value="" required>
+                    <input name="first_name" type="text" autocomplete="off" class="form-control" id="firstName" placeholder="" value="" required>
                     <div class="invalid-feedback">
                         Valid first name is required.
                     </div>
                 </div>
                 <div class="col-md-6 mb-3">
                     <label for="lastName">Last name</label>
-                    <input name="last_name" type="text" class="form-control" id="lastName" placeholder="" value="" required>
+                    <input name="last_name" type="text" autocomplete="off" class="form-control" id="lastName" placeholder="" value="" required>
                     <div class="invalid-feedback">
                         Valid last name is required.
                     </div>
@@ -33,27 +33,23 @@ require_once __DIR__ . '/../header.php';
 
             <div class="mb-3">
                 <label for="email">Email </label>
-                <input name="email" type="email" class="form-control" id="email" placeholder="you@example.com">
+                <input name="email" type="email" autocomplete="off" class="form-control" id="email" placeholder="you@example.com">
             </div>
 
             <div class="mb-3">
                 <label for="password">Password</label>
-                <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                <input name="password" type="password" autocomplete="off" id="password" class="form-control" placeholder="Password" required>
             </div>
 
             <div class="mb-3">
                 <label for="address">Home Address</label>
-                <input name="home_address" type="text" class="form-control" id="home-address" placeholder="1234 Main St" required>
+                <input name="home_address" type="text" autocomplete="off" class="form-control" id="user_input_autocomplete_address_home" placeholder="1234 Main St" required>
             </div>
 
             <div class="mb-3">
                 <label for="address">Work Address</label>
-                <input name="work_address" type="text" class="form-control" id="work-address" placeholder="1234 Main St" required>
+                <input name="work_address" type="text" autocomplete="off" class="form-control" id="user_input_autocomplete_address_work" placeholder="1234 Main St" required>
             </div>
-            <input name="home_geo_lat" type="hidden" value="49.6365498">
-            <input name="home_geo_long" type="hidden" value="6.0097661">
-            <input name="work_geo_lat" type="hidden" value="49.6318168">
-            <input name="work_geo_long" type="hidden" value="6.1692465">
 
             <hr class="mb-4">
             <button class="btn btn-primary btn-lg btn-block" type="submit">Register</button>
--- web/media/js/apiClient.js
+++ web/media/js/apiClient.js
@@ -32,6 +32,38 @@ $('.form-signin').on('submit', function (e) {
     });
 });
 
+$("#form-register").on('submit', function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: 'POST',
+        url: document.api_url + 'api/user/register',
+        cache: false,
+        data: JSON.stringify(
+            {
+                first_name: $("#firstName").val(),
+                last_name: $("#lastName").val(),
+                email: $("#email").val(),
+                password: $("#password").val(),
+                home_address: $("#user_input_autocomplete_address_home").val(),
+                work_address: $("#user_input_autocomplete_address_work").val(),
+                home_geo_lat: homeLat,
+                home_geo_long: homeLong,
+                work_geo_long: workLong,
+                work_geo_lat: workLat
+            }
+        ),
+        dataType: "json",
+        contentType: "application/json"
+    })
+    .fail(function(result) {
+        alert('Invalid data, try again.');
+    })
+    .done(function(result) {
+        localStorage.setItem('userData', JSON.stringify(result));
+        window.location.href = '/?action=dashboard';
+    });
+});
+
 $('#btn-logout').on('click', function (e) {
     localStorage.removeItem('userData');
     window.location.href = '/';
