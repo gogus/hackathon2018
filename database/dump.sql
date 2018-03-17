efd63661c540189b017a4640e7c15e8cd9020413
rowebsoft <rowebsoft@gmail.com>
2018-03-17 00:24:11 +0000

    modified the dump and creation of address

--- database/dump.sql
+++ database/dump.sql
@@ -18,12 +18,16 @@ CREATE TABLE `address`
   `user_id` VARCHAR(100) NOT NULL,
   `home_address` VARCHAR(200) NOT NULL,
   `work_address` VARCHAR(200) NOT NULL,
-  `home_geo` VARCHAR(100) NOT NULL,
-  `work_geo` VARCHAR(100) NOT NULL
+  `home_geo_lat` VARCHAR(100) NOT NULL,
+  `home_geo_long` VARCHAR(100) NOT NULL,
+  `work_geo_lat` VARCHAR(100) NOT NULL,
+  `work_geo_long` VARCHAR(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE utf8_unicode_ci;
 
+DROP TABLE address;
+
 INSERT INTO `address` (`user_id`, `home_address`, `work_address`, `home_geo`, `work_geo`) VALUES
-('725a7f14-ed1a-45e2-b82c-24736c673429', 'Eurener Straße, Trier, Germany', '44 Avenue John F. Kennedy, Luxembourg', '', ''),
-('2929d8a4-7c3a-4fbc-a564-126aece71447', "Route d'Arlon, Luxembourg", '44 Avenue John F. Kennedy, Luxembourg', '', ''),
-('e1346b39-671c-4f46-93e6-41b70a482594', 'Luxemburger Straße, Trier, Germany', '44 Avenue John F. Kennedy, Luxembourg', '', ''),
-('221d67d2-04dc-4993-a243-591661ad8642', 'Luxembourg Central Station, Luxembourg City, Luxembourg', '44 Avenue John F. Kennedy, Luxembourg', '', '');
+('725a7f14-ed1a-45e2-b82c-24736c673429', 'Eurener Straße, Trier, Germany', '44 Avenue John F. Kennedy, Luxembourg', '', '','', ''),
+('2929d8a4-7c3a-4fbc-a564-126aece71447', "Route d'Arlon, Luxembourg", '44 Avenue John F. Kennedy, Luxembourg', '', '','', ''),
+('e1346b39-671c-4f46-93e6-41b70a482594', 'Luxemburger Straße, Trier, Germany', '44 Avenue John F. Kennedy, Luxembourg', '', '','', ''),
+('221d67d2-04dc-4993-a243-591661ad8642', 'Luxembourg Central Station, Luxembourg City, Luxembourg', '44 Avenue John F. Kennedy, Luxembourg', '', '','', '');
