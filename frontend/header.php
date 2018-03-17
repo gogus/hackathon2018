28bdf2b4fd734a631a75d0bf29c2ad5c881d9425
Mikolaj Gogula <mikolaj.gogula@gmail.com>
2018-03-17 10:47:27 +0100

    Login pointing to API.

--- frontend/header.php
+++ frontend/header.php
@@ -13,4 +13,8 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" rel="stylesheet">
     <link href="/media/css/bootstrap.min.css" rel="stylesheet">
     <link href="/media/css/work.css" rel="stylesheet">
+
+    <script>
+        <?php require_once __DIR__ . '/config.php'; ?>
+    </script>
 </head>
